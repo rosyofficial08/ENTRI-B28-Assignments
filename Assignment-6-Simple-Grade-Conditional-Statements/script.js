8869cbfbf22124f2ab09ac5179317df7bb5bcf72
rosyofficial08 <rosyofficial08@gmail.com>
2026-03-08 23:16:29 +0530

    Simple Gradde Conditional Satements

---
+++ Assignment-6-Simple-Grade-Conditional-Statements/script.js
@@ -0,0 +1,23 @@
+
+
+
+let Total = 450;
+
+if(Total >= 250) {
+    if(Total >= 500 && Total <= 600) {
+        console.log("Grade A");
+    } 
+    else if(Total >= 400 && Total < 500) {
+        console.log("Grade B");
+    } 
+    else if(Total >= 300 && Total < 400) {
+        console.log("Grade C");
+    } 
+    else {
+        console.log("Grade D");
+    }
+} 
+
+else {
+    console.log("Fail");
+}
